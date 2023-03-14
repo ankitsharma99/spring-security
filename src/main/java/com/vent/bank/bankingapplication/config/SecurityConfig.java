0db27382364807104702b978994d60cc59204467
Ankit Sharma <sharmankit9593@gmail.com>
2023-03-14 16:36:58 +0530

    Added NoOpPasswordEncoder

--- src/main/java/com/vent/bank/bankingapplication/config/SecurityConfig.java
+++ src/main/java/com/vent/bank/bankingapplication/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -22,17 +24,20 @@ public class SecurityConfig {
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
+        UserDetails admin = User.withUsername("admin")
                 .password("12345")
                 .authorities("admin")
                 .build();
 
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
+        UserDetails user = User.withUsername("user")
                 .password("12345")
                 .authorities("read")
                 .build();
         return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder () {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
